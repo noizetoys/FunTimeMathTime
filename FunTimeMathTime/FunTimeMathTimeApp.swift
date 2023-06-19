2dd3d8b0aef380e936554bfb08547a1af0048418
James Majors <noizetoys@yahoo.com>
2023-06-18 20:34:27 -0400

    Initial Commit

---
+++ FunTimeMathTime/FunTimeMathTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunTimeMathTimeApp.swift
+//  FunTimeMathTime
+//
+//  Created by Apple User on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FunTimeMathTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
